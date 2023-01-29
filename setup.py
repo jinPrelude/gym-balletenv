daf17703defa349a35164413dbfa6e538e8dca2d
정의진 <jin.Prelude@gmail.com>
2023-01-29 01:54:16 +0000

    Add wrappers & update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="gym_balletenv",
     version="0.0.1",
-    install_requires=["gym==0.23.1", "pycolab==1.2", "absl-py"],
+    install_requires=["gym==0.23.1", "pycolab==1.2", "absl-py", "opencv-python", "imageio", "pygifsicle"],
 )
