ff5eeeed52e3968652928632c360cea10deecbed
Euijin Jeong <jin.Prelude@gmail.com>
2023-07-15 07:29:03 +0000

    Fix pycolab install_requires
    fix error on python>=3.10

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
     name="gym_balletenv",
     version="0.0.2",
-    install_requires=["gymnasium==0.27.1", "pycolab==1.2", "absl-py", "opencv-python", "moviepy"],
+    install_requires=[
+        "gymnasium==0.27.1",
+        "pycolab @ git+https://github.com/deepmind/pycolab.git",
+        "absl-py",
+        "opencv-python",
+        "moviepy"
+        ],
 )
